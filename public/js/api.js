d9467424b27dc5e8a69543268ca4c50cc8d4a14f
Tyler Ferrara <tylerferrara123@gmail.com>
2017-05-13 17:12:24 -0400

    Setup AJAX data handling to main app

--- public/js/api.js
+++ public/js/api.js
@@ -1,13 +1,57 @@
-console.log("API.js is here");
-
 $(function (){
 
-  $.ajax({
-    type: 'DELETE',
-    url: '/api/5916300320df7815a05c069a/tasks/5916fb1fe83c2b2ebc733fb1',
-    success: function(data) {
-      console.log(data);
-    }
+  $(document).ready(function(){
+    $.ajax({
+      type: 'GET',
+      url: '/api/' + userId + '/tasks',
+      success: function(data) {
+        console.log(data);
+        // add it to visable html
+      }
+    });
   });
+  // $.ajax({
+  //   type: 'DELETE',
+  //   url: '/api/5916300320df7815a05c069a/tasks/5916fb1fe83c2b2ebc733fb1',
+  //   success: function(data) {
+  //     console.log(data);
+  //   }
+  // });
+
+
+
+$(".newTaskInput").keypress(function(e){
+  if(e.which == 13){
+      var input = $(this).val();
+      $.ajax({
+        type: 'POST',
+        url: '/api/' + userId + '/tasks?n=' + input + '&da=2&do=false',
+        success: function(data) {
+          console.log(data);
+        }
+      });
+      $(this).val('');
+      $.ajax({
+        type: 'GET',
+        url: '/api/' + userId + '/tasks',
+        success: function(data) {
+          console.log(data);
+          // add it to visable html
+        }
+      });
+  }
+});
+//
+// $(".UpdateTaskButton").click(function(){
+//   $.ajax({
+//     type: 'POST',
+//     url: '/api/5916300320df7815a05c069a/tasks?n=' + input + '&da=2&do=false',
+//     success: function(data) {
+//       console.log(data);
+//     }
+//   });
+// });
+
+
 
 });
